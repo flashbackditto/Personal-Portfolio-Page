49548fa0616c9a736d3991fc2206404e56d5e2de
Adam <flashbackditto@users.noreply.github.com>
2019-03-05 22:34:24 -0800

    php script for mailsend

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $mailFrom = $_POST['email'];
+  $message = $_POST['comments'];
+
+$mailTo = "adamleewebdev@yahoo.com"; //cant sent to gmail
+$headers = "From: $mailFrom";
+$txt = "You have received an email from $name.\n\n $message;
+
+  mail($mailTo, $txt, $headers);
+  header("Location: index.php?mailsend");
+  echo "Thank You!";
+};
+?>
