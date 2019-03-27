2ec91b2c08ad4498e95afeabaca7f4149c3e692a
Adam <flashbackditto@users.noreply.github.com>
2019-03-26 22:31:22 -0700

    rearranged mail function arguments

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $mailTo = "adamleewebdev@yahoo.com"; //cant sent to gmail
 $headers = "From: $mailFrom";
 $txt = "You have received an email from $name.\n\n $message";
 
-  mail($mailTo, $txt, $headers);
+  mail($mailTo, $headers, $txt);
   header("Location: index.php?mailsend");
   echo "Thank You!";
 };
