3016abf62e67df1f2a3999ace529f48b6b5c72ea
Adam <flashbackditto@users.noreply.github.com>
2019-03-26 20:38:19 -0700

    fixed a syntax error in index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])) {
 
 $mailTo = "adamleewebdev@yahoo.com"; //cant sent to gmail
 $headers = "From: $mailFrom";
-$txt = "You have received an email from $name.\n\n $message;
+$txt = "You have received an email from $name.\n\n $message";
 
   mail($mailTo, $txt, $headers);
   header("Location: index.php?mailsend");
